161c57503d870af7f1f01e56b90f5bc02a9062b1
saqibraza <saqibraza.dev@gmail.com>
2023-10-13 17:16:40 +0500

    Stats section improved

--- app/(components)/stats/stats.js
+++ app/(components)/stats/stats.js
@@ -1,19 +1,20 @@
 "use client"
 import React from 'react'
+import Link from 'next/link'
 import { useEffect, useState } from 'react';
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../../../config/firebase.js';
 
 export default function Stats() {
 
-  const [count, setCount] = useState(0);
+  const [studentCount, setStudentCount] = useState(0);
 
   useEffect(() => {
     // Fetch the total number of documents in the "EnrolledStudents" collection
     const fetchDocumentCount = async () => {
       const collectionRef = collection(db, 'EnrolledStudents');
       const querySnapshot = await getDocs(collectionRef);
-      setCount(querySnapshot.size);
+      setStudentCount(querySnapshot.size);
     };
 
     fetchDocumentCount();
@@ -28,10 +29,19 @@ export default function Stats() {
 
 
 
-      <div className="py-14 flex flex-col md:flex-row md:justify-between w-full space-y-6 md:space-y-0">
-        <div className="bg-slate-200 h-14 text-center flex items-center px-6 rounded-xl">Total Students:  {count}</div>
-        <div className="bg-slate-200 h-14 text-center flex items-center px-6 rounded-xl">Offered Programs:{count}</div>
-        <div className="bg-slate-200 h-14 text-center flex items-center px-6 rounded-xl">Open Programs:{count}</div>
+      <div className="py-14 flex flex-col md:flex-row md:justify-between w-full space-y-6 md:space-y-0 text-center">
+
+        <Link href="/students" className='bg-white hover:bg-gray-200 text-gray-800 md:font-semibold py-2 px-6 border border-gray-400 rounded shadow max-w-xs flex items-center '>
+          Total Students:  {studentCount}
+        </Link>
+
+        <Link href="/cources" className='bg-white hover:bg-gray-200 text-gray-800 md:font-semibold py-2 px-6 border border-gray-400 rounded shadow max-w-xs flex items-center'>
+          Offered Programs:  {studentCount}
+        </Link>
+
+        <Link href="/students/addStudent" className='bg-white hover:bg-gray-200 text-gray-800 md:font-semibold py-2 px-6 border border-gray-400 rounded shadow max-w-xs flex items-center'>
+          Open Programs:  {studentCount}
+        </Link>
       </div>
 
 
